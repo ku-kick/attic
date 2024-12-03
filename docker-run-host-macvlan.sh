c97816203b0b4f4b4ab18e7aed30bfb86e917fd7
DmitryMurashov <dmtr.murashov@gmail.com>
2024-12-02 22:45:18 -0700

    docker: refactoring

--- docker-run-host-macvlan.sh
+++ docker-run-host-macvlan.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# Network
 IPNET=192.168.1.0/24
+# IP of the container
 IP=192.168.1.90
+# Host gateway
 GATE=192.168.1.100
+# Name for the network
 NETNAME=docker_dummy
-docker network create -d macvlan --subnet=$IPNET --gateway=$GATE -o parent=eth_dummy $NETNAME
-#docker run -it --rm --network $NETNAME --ip $IP ubuntu-pimped /bin/bash
-docker run -it --rm --network $NETNAME ubuntu-pimped /bin/bash
+# Interface name
+IFNAME=ens33
+docker network create -d macvlan --subnet=$IPNET --gateway=$GATE -o parent=$IFNAME $NETNAME
+docker run -it --rm --network $NETNAME --ip $IP ubuntu-pimped /bin/bash
+#docker run -it --rm --network $NETNAME ubuntu-pimped /bin/bash
