061c6a3be56d8bcd56fb537f0b869f04ed202b8d
DmitryMurashov <dmtr.murashov@gmail.com>
2024-12-29 11:36:02 +0300

    [net] Ref | Replaced magic numbers w/ variables

--- create-virtual-ethernet-namespace.sh
+++ create-virtual-ethernet-namespace.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-NAMESPACE_NAME=dummyns
+NAMESPACE_NAME=${NAMESPACE_NAME:-dummyns}
+ETHNAME1=${ETHNAME1:-eth_dummy}
+ETHNAME2=${ETHNAME2:-eth_dummy1}
+IPNET1=${IPNET1:-"10.10.1.100/24"}
+IPNET2=${IPNET2:-"10.10.1.200/24"}
 
 # Create
-sudo ip link add eth_dummy type veth peer name eth_dummy1
+sudo ip link add $ETHNAME1 type veth peer name $ETHNAME2
 
 # Handle the first end
 # Assign IP
-sudo ip addr add 10.10.1.100/24 dev eth_dummy
+sudo ip addr add $IPNET1 dev $ETHNAME1
 # Bring up
-sudo ip link set eth_dummy promisc on
+sudo ip link set $ETHNAME1 promisc on
 
 # Handle the second end
-sudo ip addr add 10.10.1.200/24 dev eth_dummy1
-sudo ip link set eth_dummy1 promisc on
+sudo ip addr add $IPNET2 dev $ETHNAME2
+sudo ip link set $ETHNAME2 promisc on
 # Hide under namespace
 sudo ip netns add $NAMESPACE_NAME
-sudo ip link set eth_dummy1 netns $NAMESPACE_NAME
+sudo ip link set $ETHNAME2 netns $NAMESPACE_NAME
 
 # Run both ends
-sudo ip link set eth_dummy up
-sudo ip netns exec $NAMESPACE_NAME ip link set eth_dummy1 up
+sudo ip link set $ETHNAME1 up
+sudo ip netns exec $NAMESPACE_NAME ip link set $ETHNAME2 up
 
 # Verify both ends
-ip addr show eth_dummy
-sudo ip netns exec $NAMESPACE_NAME ip addr show eth_dummy1
+ip addr show $ETHNAME1
+sudo ip netns exec $NAMESPACE_NAME ip addr show $ETHNAME2
