a394452cdb41746393ef93a0af51921d809e7f3d
DmitryMurashov <dmtr.murashov@gmail.com>
2024-12-29 13:34:08 +0300

    [net] Impl | An attempt to create a ns w/ shared interfaces. Futile: unsharing into a pre-existing network ns removes previously existed veth interfaces. Semi-complete

--- remove-virtual-ethernet-namespace.sh
+++ remove-virtual-ethernet-namespace.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-NAMESPACE_NAME=dummyns
+NAMESPACE_NAME=${NAMESPACE_NAME:-dummyns}
+ETHNAME1=${ETHNAME1:-eth_dummy}
+ETHNAME2=${ETHNAME2:-eth_dummy1}
 
-sudo ip link delete eth_dummy
-sudo ip netns exec $NAMESPACE_NAME ip link delete eth_dummy1
+sudo ip link delete $ETHNAME1
+sudo ip netns exec $NAMESPACE_NAME ip link delete $ETHNAME2
 sudo ip netns delete $NAMESPACE_NAME
--- smuggler.sh
+++ smuggler.sh
@@ -7,39 +7,71 @@
 
 # TODO:
 # 1. pack fs as a file, use "loop" bind;
-# 2. Throw in an ETH interface that can be used for networking;
-# 3. Test docker 
-# 4. Apply cgroups CPU restrictions 
+# 2. Throw in an ETH interface that can be used for networking; 
+#    FOR SOME REASON, UNSHARING INTO NAMESPACE REMOVES PREVIOUSLY EXISTED INTERFACES
+# 3. Test docker
+# 4. Apply cgroups CPU restrictions
 # 5. Integrate w/ mininet (, or containernet)
 
 HERE=$(realpath $(dirname ${BASH_SOURCE[0]}))
-NAME=bankrobber6
+NAME=bankrobber21
 ROOTDIR=$HERE/$NAME
 ROOTFS=$ROOTDIR/debian
 MOUNTDIR=/mnt/$NAME
+NETNS_FILE_PATHS=/var/run/netns
 
 mkdir -p $ROOTDIR
 
+echo Bootstrapping debian FS
 mkdir -p $ROOTFS
-sudo debootstrap bookworm $ROOTFS http://deb.debian.org/debian/
+#sudo debootstrap bookworm $ROOTFS http://deb.debian.org/debian/
+ln -s $(realpath debian) $NAME/debian
+ROOTFS=$ROOTFS/debian
 
+echo Creating namespaces
 sudo mkdir /mnt/$NAME -p
 sudo mount --bind $ROOTDIR $MOUNTDIR
 sudo mount --make-private $MOUNTDIR
-touch $MOUNTDIR/$NAME-mount
-touch $MOUNTDIR/$NAME-uts
-touch $MOUNTDIR/$NAME-pid
-touch $MOUNTDIR/$NAME-net
+touch $ROOTDIR/$NAME-mount
+touch $ROOTDIR/$NAME-uts
+touch $ROOTDIR/$NAME-pid
+# Network namespace
+sudo ip netns add $NAME-net
+ln -s $NETNS_FILE_PATHS/$NAME-net $MOUNTDIR/$NAME-net 
+# IPC namespace
 touch $MOUNTDIR/$NAME-ipc
 
-sudo unshare --uts=$MOUNTDIR/$NAME-uts \
-	--pid=$MOUNTDIR/$NAME-pid \
-	--net=$MOUNTDIR/$NAME-net \
-	--mount=$MOUNTDIR/$NAME-mount \
-	--ipc=$MOUNTDIR/$NAME-ipc \
-	--fork
+echo mounting proc
+sudo mount -t proc none $ROOTFS/proc
+echo mounting sysfs
+sudo mount -t sysfs none $ROOTFS/sys
+echo mounting dev
+sudo mount -o rbind /dev $ROOTFS/dev
 
-mount -t proc none $ROOTFS/proc
-mount -t sysfs none $ROOTFS/sys
-mount -o bind /dev $ROOTFS/dev
-chroot $ROOTFS
+# Create veth-s
+echo Creating virtual network within namespaces
+NAMESPACE_NAME=$NAME-net create-virtual-ethernet-namespace.sh
+sudo ip netns exec $NAME-net ip addr show eth_dummy1
+
+echo Unsharing into namespaces, chrooting
+#sudo unshare --uts=$MOUNTDIR/$NAME-uts \
+#	--pid=$MOUNTDIR/$NAME-pid \
+#	--net=$MOUNTDIR/$NAME-net \
+#	--mount=$MOUNTDIR/$NAME-mount \
+#	--ipc=$MOUNTDIR/$NAME-ipc \
+#	/bin/bash -c "echo chrooting into the rootfs ; chroot $ROOTFS"
+sudo unshare \
+	--net=/var/run/netns/$NAME-net \
+	--keep-caps \
+	--mount=$MOUNTDIR/$NAME-mount \
+	/bin/bash -c "echo chrooting into the rootfs ; chroot $ROOTFS"
+
+if false ; then
+	echo Chrooting into the fs
+	sudo chroot $ROOTFS
+fi
+
+if true ; then 
+	echo Removing veth
+	NAMESPACE_NAME=$NAME-net remove-virtual-ethernet-namespace.sh
+fi
